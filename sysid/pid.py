40eae6aa7b5ee09baeab57b60fb23c88241677e3
Mike Kroutikov <pgmmpk@gmail.com>
2014-04-27 13:06:29 -0400

    stabilising motor speed with PID controller

---
+++ sysid/pid.py
@@ -0,0 +1,22 @@
+"""
+PID controller
+"""
+
+
+class PID:
+
+    def __init__(self, Kp, Ki=0, Kd=0, x0=0):
+        self.Kp = Kp
+        self.Ki = Ki
+        self.Kd = Kd
+
+        self._x_prev = x0
+        self._acc = 0
+
+    def feed(self, x):
+        self._acc += x
+
+        out = self.Kp * x + self.Ki * self._acc + self.Kd * (x - self._x)
+        self._x_prev = x
+
+        return out

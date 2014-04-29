f736816f5a80657ff690a49b0e588be99f1f0978
Mike Kroutikov <pgmmpk@gmail.com>
2014-04-28 21:18:43 -0400

    tuning sign detection

--- sysid/controller_plot.py
+++ sysid/controller_plot.py
@@ -2,10 +2,10 @@ import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
 
-    filename = 'controller-wa-20.csv'
-    #filename = 'controller-wa-40.csv'
-    #filename = 'controller-wa-60.csv'
-    #filename = 'controller-wa-80.csv'
+    #filename = 'controller-wa-20.csv'
+    filename = 'controller-wa-40.csv'
+    filename = 'controller-wa-60.csv'
+    filename = 'controller-wa-80.csv'
     #filename = 'controller-wa-100.csv'
     #filename = 'controller-wa-120.csv'
     #filename = 'controller-wa-150.csv'
@@ -20,9 +20,29 @@ if __name__ == '__main__':
     speed = [d[2] for d in data]
     control = [d[3] for d in data]
 
+    DT = 0.05
+    ALPHA = 2.0
+
+    pspeed = []
+    prev_s = None
+    predicted_speed = 0
+    direction = 1
+    for r, s in zip(reference, speed):
+        old_speed = predicted_speed
+        predicted_speed += DT * (r * direction - predicted_speed + ALPHA * (abs(s) - predicted_speed))
+        if old_speed * predicted_speed < 0 or 0 < predicted_speed < 1.0:
+            print 'sign changed at', s
+            direction = 0
+            predicted_speed = 0
+        elif direction == 0 and predicted_speed >= 1.0:
+            direction = 1 if r >= 0 else -1
+
+        pspeed.append(predicted_speed * direction)
+
     plt.plot(timer, reference)
     plt.plot(timer, speed, color='g')
     plt.plot(timer, control, color='r')
+    plt.plot(timer, pspeed, color='m')
     plt.title('Speed')
 
     plt.show()

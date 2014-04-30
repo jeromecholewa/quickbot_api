9c0bd0383afe21e8bb95d3914f6a742d95477ff9
Mike Kroutikov <pgmmpk@gmail.com>
2014-04-29 20:08:49 -0400

    code cleanup

---
+++ qb_client.py
@@ -0,0 +1,90 @@
+import contextlib
+import re
+import socket
+
+
+class QBClient:
+    """
+    Implements QB interface. This is a proxy to the remote QB object running on the robot side.
+    """
+
+    BUFFER_SIZE = 1024
+
+    DEFAULT_PORT = 5005
+
+    def __init__(self, robot_ip, base_ip, port=DEFAULT_PORT):
+        self.robot_ip = robot_ip
+        self.base_ip = base_ip
+        self.port = port
+        self._sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self._sock.bind((self.base_ip, self.port))
+        self._sock.settimeout(0.5)
+
+    def close(self):
+        self._sock.close()
+
+    def check(self):
+        reply = self._send_recv("$CHECK*\n")
+        return reply.startswith("Hello from QuickBot")
+
+    def get_ticks(self):
+        reply = self._send_recv("$ENVAL?*\n")
+        return parse_tuple(reply)
+
+    def reset_ticks(self):
+        self._send_recv("$RESET*\n", False)
+
+    def set_speed(self, left_val, right_val):
+        self._send_recv("$PWM=%s,%s*\n" % (left_val, right_val), False)
+
+    def get_speed(self):
+        reply = self._send_recv("$PWM?*\n")
+        return parse_tuple(reply)
+
+    def get_ir(self):
+        reply = self._send_recv('$IRVAL?*\n')
+        return parse_tuple(reply)
+
+    def _send_recv(self, message, expect_reply=True):
+
+        for _ in range(3):  # re-try count
+            try:
+                self._sock.sendto(message, (self.robot_ip, self.port))
+
+                if not expect_reply:
+                    return
+
+                reply, _ = self._sock.recvfrom(QBClient.BUFFER_SIZE)
+                return reply
+            except socket.timeout:
+                pass
+
+        raise socket.timeout()
+
+    @classmethod
+    @contextlib.contextmanager
+    def connect(cls, robot_ip, base_ip, port=DEFAULT_PORT):
+
+        client = QBClient(robot_ip, base_ip, port)
+
+        try:
+            client.check()
+
+            yield client
+
+        finally:
+            client.close()
+
+
+def parse_tuple(reply):
+    reply = re.sub(r'[\[\],]+', ' ', reply).strip()
+    return tuple(float(x) for x in reply.split())
+
+
+if __name__ == '__main__':
+    import config
+
+    with QBClient.connect(config.ROBOT_IP, config.BASE_IP, config.PORT) as qb:
+
+        for _ in range(1000):
+            print qb.get_ir()
---
+++ qb_server.py
@@ -0,0 +1,105 @@
+import time
+import re
+import socket
+from qb import QB
+from sysid.controller import BotController
+
+
+class QBServer(QB):
+    """The QuickBot Class. Just a UDP proxy on top of QB class functionality"""
+
+    SAMPLE_TIME = 0.01  # 100Hz
+
+    def __init__(self, config):
+        QB.__init__(self, config)
+
+        self._sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self._sock.settimeout(self.SAMPLE_TIME)
+
+        # Set IP addresses
+        self.base_ip  = config.BASE_IP
+        self.robot_ip = config.ROBOT_IP
+        self.port     = config.PORT
+
+        self._sock.bind((self.robot_ip, self.port))
+
+    def stop(self):
+        QB.stop(self)
+        self._sock.close()
+
+    def recv_line(self):
+        try:
+            return self._sock.recv(1024)
+        except socket.timeout:
+            pass
+
+    def send_line(self, line):
+        self._sock.sendto(line, (self.base_ip, self.port))
+
+    @classmethod
+    def run(cls, config):
+
+        qb = QBServer(config)
+        qb.start()
+
+        print 'QuickBot is ready.'
+        print 'Base IP is', qb.base_ip
+        print 'Robot IP is', qb.robot_ip
+        print 'Port is', qb.port
+
+        try:
+
+            cmd_buffer = ''
+            while True:
+                line = qb.recv_line()
+                if not line:
+                    continue
+
+                mtc = re.match(r'\$(?P<CMD>[A-Z]{3,})(?P<SET>=?)(?P<QUERY>\??)(?(2)(?P<ARGS>.*)).*\*', line)
+                if not mtc:
+                    print 'Unexpected command, ignoring:', line
+                    continue
+
+                if mtc.group('CMD') == 'CHECK':
+                    qb.send_line('Hello from QuickBot\n')
+
+                elif mtc.group('CMD') == 'PWM':
+                    if mtc.group('QUERY'):
+                        qb.send_line('[%s,%s]\n' % qb.get_speed())
+
+                    elif mtc.group('SET') and mtc.group('ARGS'):
+                        args = mtc.group('ARGS')
+                        parts = args.split(',')
+                        speed_left = float(parts[0].strip())
+                        speed_right = float(parts[1].strip())
+                        qb.set_speed(speed_left, speed_right)
+
+                    else:
+                        print 'Malformed PWM command, ignoring:', line
+
+                elif mtc.group('CMD') == 'IRVAL':
+                    if mtc.group('QUERY'):
+                        qb.send_line('[%s, %s, %s, %s, %s]\n' % qb.get_ir())
+
+                elif mtc.group('CMD') == 'ENVAL':
+                    if mtc.group('QUERY'):
+                        qb.send_line('[%s, %s]\n' % qb.get_ticks())
+
+                elif mtc.group('CMD') == 'ENVEL':
+                    if mtc.group('QUERY'):
+                        qb.send_line('[%s, %s]\n' % qb.get_speed())
+
+                elif mtc.group('CMD') == 'RESET':
+                    qb.reset_ticks()
+
+                elif mtc.group('CMD') == 'END':
+                    break
+
+        finally:
+            qb.stop()
+
+
+if __name__ == '__main__':
+    import config
+
+    QBServer.run(config)

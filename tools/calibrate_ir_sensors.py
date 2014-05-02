79c30aeb9b0c92ea8a43649c9c3125370a7616fe
Mike Kroutikov <pgmmpk@gmail.com>
2014-05-01 21:53:16 -0400

    code to calibrate IR sensors

---
+++ tools/calibrate_ir_sensors.py
@@ -0,0 +1,37 @@
+import beaglebone_pru_adc as adc
+import time
+import collections
+import config
+from robot.motor import Motors
+
+if __name__ == '__main__':
+
+    adc = adc.Capture()
+
+    ir_pins = config.IR_PINS
+
+    adc.start()
+
+    stat = collections.defaultdict(list)
+
+    print '\x08'
+    for _ in range(1000):
+        time.sleep(0.005)
+        values = adc.values
+        for i, pin in enumerate(ir_pins):
+            stat[i].append(values[pin])
+
+    adc.stop()
+    adc.wait()
+
+    _, min0, max0, _, _ = adc.encoder0_values
+    _, min1, max1, _, _ = adc.encoder1_values
+
+    adc.close()
+
+    for i in range(len(ir_pins)):
+        mean = sum(stat[i]) / len(stat[i])
+        min_ = min(stat[i])
+        max_ = max(stat[i])
+
+        print '#%d: RANGE: %4.2lf -- %4.2lf, MEAN: %4.2lf' % i

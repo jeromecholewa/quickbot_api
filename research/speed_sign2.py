eb287e1f84a1e8f6aa9cac5eb5e8965dbdda725a
Mike Kroutikov <pgmmpk@gmail.com>
2014-05-04 19:55:55 -0400

    cosmetic

--- research/speed_sign2.py
+++ research/speed_sign2.py
@@ -4,6 +4,7 @@ import time
 
 if __name__ == '__main__':
 
+	SPEED = 40.0
 	ctrl = BotController(config)
 	ctrl.start()
 
@@ -14,23 +15,23 @@ if __name__ == '__main__':
 	for _ in range(300):
 		time.sleep(0.01)
 		ctrl.on_timer()
-		
+
 		data.append((ctrl.timer, 
-			ctrl.actual_speed[0], 
-			ctrl.actual_speed[1], 
-			ctrl.ticks[0], 
+			ctrl.actual_speed[0],
+			ctrl.actual_speed[1],
+			ctrl.ticks[0],
 			ctrl.ticks[1],
 			ctrl._left.torque,
 			ctrl._right.torque,
 			torque))
 
 		if _ == 50:
-			torque = 50
-			ctrl.run(50, 50)
+			torque = SPEED
+			ctrl.run(SPEED, SPEED)
 
 		if _ == 150:
-			torque = -50
-			ctrl.run(-50, -50)
+			torque = -SPEED
+			ctrl.run(-SPEED, -SPEED)
 
 		if _ == 250:
 			torque = 0

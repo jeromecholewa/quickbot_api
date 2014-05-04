8b0857f1cdd28a8700854fc94f271d1d3bc4607f
Mike Kroutikov <pgmmpk@gmail.com>
2014-05-04 18:05:39 -0400

    plotting actual torque

--- research/speed_sign2_plot.py
+++ research/speed_sign2_plot.py
@@ -6,13 +6,15 @@ with open('speed_sign2.csv') as f:
 	for line in f:
 		data.append(tuple(float(x) for x in line.strip().split(', ')))
 
-timer, speed_left, speed_right, ticks_left, ticks_right, torque = zip(*data)
+timer, speed_left, speed_right, ticks_left, ticks_right, torque_left, torque_right, torque = zip(*data)
 timer = [x / 120000 for x in timer]
 
 plt.plot(timer, speed_left, color='r', label='speed (left)')
 plt.plot(timer, speed_right, color='g', label='speed (right)')
 plt.plot(timer, ticks_right, color='m', label='ticks (left)')
 plt.plot(timer, ticks_left, color='c', label='ticks (right)')
+plt.plot(timer, torque_left, label='torque (left)')
+plt.plot(timer, torque_right, label='torque (right)')
 plt.plot(timer, torque, color='b', label='torque')
 plt.legend(loc=3)
 plt.ylim([-80, 100])

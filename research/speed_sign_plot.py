f53a819adba67a229bd303bc1452d847e1b78403
Mike Kroutikov <pgmmpk@gmail.com>
2014-05-04 15:11:20 -0400

    better plot

--- research/speed_sign_plot.py
+++ research/speed_sign_plot.py
@@ -3,13 +3,15 @@ import matplotlib.pyplot as plt
 data = []
 with open('speed_sign.csv') as f:
 	for line in f:
-		data.append(tuple(float(x) for x in line.split()))
+		data.append(tuple(float(x) for x in line.strip().split(', ')))
 
 timer, speed_left, speed_right, ticks_left, ticks_right, torque = zip(*data)
 
-plt.plot(timer, speed_left, color='r')
-plt.plot(timer, speed_right, color='g')
-plt.plot(timer, ticks_right, color='m')
-plt.plot(timer, ticks_left, color='c')
-plt.plot(timer, torque, color='b')
+plt.plot(timer, speed_left, color='r', label='speed (left)')
+plt.plot(timer, speed_right, color='g', label='speed (right)')
+plt.plot(timer, ticks_right, color='m', label='ticks (left)')
+plt.plot(timer, ticks_left, color='c', label='ticks (right)')
+plt.plot(timer, torque, color='b', label='torque')
+plt.legend(loc=2)
+plt.ylim([-60, 160])
 plt.show()

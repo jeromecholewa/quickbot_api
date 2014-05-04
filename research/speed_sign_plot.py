a6bf6ebaf3f972443f8a5e48555aae4d3e765713
Mike Kroutikov <pgmmpk@gmail.com>
2014-05-04 17:00:37 -0400

    cosmetic

--- research/speed_sign_plot.py
+++ research/speed_sign_plot.py
@@ -6,12 +6,13 @@ with open('speed_sign.csv') as f:
 		data.append(tuple(float(x) for x in line.strip().split(', ')))
 
 timer, speed_left, speed_right, ticks_left, ticks_right, torque = zip(*data)
+timer = [x / 120000 for x in timer]
 
 plt.plot(timer, speed_left, color='r', label='speed (left)')
 plt.plot(timer, speed_right, color='g', label='speed (right)')
 plt.plot(timer, ticks_right, color='m', label='ticks (left)')
 plt.plot(timer, ticks_left, color='c', label='ticks (right)')
 plt.plot(timer, torque, color='b', label='torque')
-plt.legend(loc=2)
-plt.ylim([-60, 160])
+plt.legend(loc=3)
+plt.ylim([-80, 100])
 plt.show()
